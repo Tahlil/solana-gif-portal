7fd5f8e033b1552e30acd9308d0a2d37a60a3ca2
tahlil <bsse0803@iit.du.ac.bd>
2022-06-26 23:57:42 +0600

    Update add gif method

--- programs/gifportal/src/lib.rs
+++ programs/gifportal/src/lib.rs
@@ -15,6 +15,13 @@ pub mod gifportal {
 
     pub fn add_gif(ctx: Context<AddGif>) -> ProgramResult{
         let base_account = &mut ctx.accounts.base_account;
+        let user = &mut ctx.accounts.user;
+        let item = ItemStruct{
+            gif_link: gif_link.to_string(),
+            user_address: *user.to_account_info().key
+        }
+
+        base_account.gif_list.push(item);
         base_account.total_gifs += 1;
         Ok(())
     }
