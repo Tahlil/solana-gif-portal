c8959fcc3db8982e197b330b1da9e5bcc2b16654
tahlil <bsse0803@iit.du.ac.bd>
2022-06-27 01:20:04 +0600

    Update program id

--- programs/gifportal/src/lib.rs
+++ programs/gifportal/src/lib.rs
@@ -1,7 +1,7 @@
 use anchor_lang::prelude::*;
 use anchor_lang::solana_program::entrypoint::ProgramResult;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("82wpSArZHYT41sZHTBzLv7FMr1anD5JCMLFfeKZ5zKU2");
 
 #[program]
 pub mod gifportal {
