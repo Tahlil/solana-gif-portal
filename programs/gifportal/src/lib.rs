8db033c4e2990e6bfc9ec91fa8412507bae33773
tahlil <bsse0803@iit.du.ac.bd>
2022-06-26 21:44:31 +0600

    Add account for the program

--- programs/gifportal/src/lib.rs
+++ programs/gifportal/src/lib.rs
@@ -8,13 +8,23 @@ pub mod gifportal {
     use super::*;
 
     pub fn start_stuff_off(ctx: Context<StartStaffOff>) -> ProgramResult {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.total_gifs =0 ;
         Ok(())
     }
     
 }
 
 #[derive(Accounts)]
-pub struct StartStaffOff {
-    
+pub struct StartStaffOff<'info> {
+    #[account(init, payer=user, space=9000)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user:Signer<'info>,
+    pub system_program: Program<'info, System>
 }
 
+#[account]
+pub struct BaseAccount {
+    pub total_gifs:u64,
+}
